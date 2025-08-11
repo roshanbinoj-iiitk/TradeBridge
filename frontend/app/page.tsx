414dc76b55cb63109d450fa86224f9c21f0fc648
Roshan Binoj <roshanbinoj2005@gmail.com>
2025-08-11 22:37:26 +0530

    NextJS Frontend Initialised

---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Welcome to TradeBridge!</h1>
+      <p className="mt-4">A student rental platform for IIIT Kottayam.</p>
+    </div>
+  );
+}
